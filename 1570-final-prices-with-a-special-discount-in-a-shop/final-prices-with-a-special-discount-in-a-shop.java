ef3e764e58053444e20170abfd2d06f0e7080203
ramireddyramakrishna0-boop <ramireddyramakrishna0@gmail.com>
2026-02-13 11:17:26 +0530

    Time: 1 ms (99.20%) | Memory: 45 MB (93.25%) - LeetSync

---
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] finalPrices(int[] prices) {
+        
+        int len = prices.length;
+
+        for(int i = 0; i < len; i++){
+
+            for(int j = i + 1; j < len; j++){
+
+                if(prices[j] <= prices[i]){
+                    prices[i] -= prices[j];
+                    break;
+                }
+            }
+        }
+
+        return prices;
+    }
+}
