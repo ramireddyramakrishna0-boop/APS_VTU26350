d03b7cc24a674ee07c4ef62c96ac29c30451be55
ramireddyramakrishna0-boop <ramireddyramakrishna0@gmail.com>
2026-02-13 11:11:54 +0530

    Time: 25 ms (82.06%) | Memory: 103.7 MB (84.93%) - LeetSync

---
+++ 739-daily-temperatures/daily-temperatures.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+
+        int[] res = new int[temperatures.length];
+        Deque<Integer> st = new ArrayDeque<>();
+
+        for (int i = 0; i < temperatures.length; i++) {
+
+            while (!st.isEmpty() && temperatures[st.peek()] < temperatures[i]) {
+                res[st.peek()] = i - st.pop();
+            }
+
+            st.push(i);
+        }
+
+        return res;
+    }
+}
