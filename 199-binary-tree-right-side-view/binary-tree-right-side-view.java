54c20af16959562ca565462b3472e354932f3106
ramireddyramakrishna0-boop <ramireddyramakrishna0@gmail.com>
2026-02-16 22:43:05 +0530

    Time: 1 ms (72.48%) | Memory: 43.8 MB (30.80%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -1,19 +1,23 @@
+import java.util.*;
+// takesoumen collection 
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
-        List<Integer> ll = new ArrayList<>();
-        if (root == null) return ll; 
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
-        while (!q.isEmpty()) {
-            int n = q.size();
-            TreeNode rv = null;
-            while(n-->0){
-                rv = q.poll();
-                if (rv.left != null) q.add(rv.left);
-                if (rv.right != null) q.add(rv.right);
+        List<Integer> result = new ArrayList<>();
+        if (root == null) return result;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode node = queue.poll();
+                if (i == levelSize - 1) result.add(node.val);
+                if (node.left != null) queue.add(node.left);
+                if (node.right != null) queue.add(node.right);
             }
-            ll.add(rv.val);
         }
-        return ll;
+
+        return result;
     }
 }
