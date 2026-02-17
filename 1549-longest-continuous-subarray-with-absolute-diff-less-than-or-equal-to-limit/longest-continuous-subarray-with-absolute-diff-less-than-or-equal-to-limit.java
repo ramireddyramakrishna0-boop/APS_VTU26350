d09136fdac8e1c59073013307e89e69c62aa1a1e
ramireddyramakrishna0-boop <ramireddyramakrishna0@gmail.com>
2026-02-17 09:17:37 +0530

    Time: 28 ms (98.36%) | Memory: 94 MB (38.23%) - LeetSync

--- 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
+++ 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
@@ -1,25 +1,40 @@
 class Solution {
     public int longestSubarray(int[] nums, int limit) {
-        Deque<Integer> maxq = new ArrayDeque<>();
-        Deque<Integer> minq = new ArrayDeque<>();
-        int n = nums.length;
-        int j = 0;
-        int ans = 0;
-        for(int i = 0; i < n; i ++){
-            while(!maxq.isEmpty() && nums[i] > maxq.peekLast())
-                maxq.pollLast();
-            maxq.addLast(nums[i]);
-            while(!minq.isEmpty() && nums[i] < minq.peekLast()){
-                minq.pollLast();
+        
+        LinkedList<Integer> increase = new LinkedList<>();
+        LinkedList<Integer> decrease = new LinkedList<>();
+
+        int max = 0;
+        int left = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            int n = nums[i];
+
+            while (increase.size() > 0 && n < increase.getLast()) {
+                increase.removeLast();
             }
-            minq.addLast(nums[i]);
-            if(maxq.peekFirst() - minq.peekFirst() > limit){
-                if(nums[j] == maxq.peekFirst()) maxq.pollFirst();
-                if(nums[j] == minq.peekFirst()) minq.pollFirst();
-                j++;
+            increase.add(n);
+
+            while (decrease.size() > 0 && n > decrease.getLast()) {
+                decrease.removeLast();
             }
-            ans = Math.max(ans, i - j + 1);
+
+            decrease.add(n);
+
+            while (decrease.getFirst() - increase.getFirst() > limit) {
+                if (nums[left] == decrease.getFirst()) {
+                    decrease.removeFirst();
+                }
+                if (nums[left] == increase.getFirst()) {
+                    increase.removeFirst();
+                }
+                left++;
+            }
+
+            int size = i - left + 1;
+            max = Math.max(max, size);
         }
-        return ans;
+
+        return max;
     }
 }
